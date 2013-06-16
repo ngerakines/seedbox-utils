62cf442ffcc0cfa80b81b4ca6617516a949a21ce
Nick Gerakines <nick@gerakines.net>
2013-06-16 00:09:51 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-seedbox-utils
-=============
+# seedbox-utils
+
+# put.sh
+
+A small script to split a file, encrypt the parts (using gpg) and put them onto s3.
+
+    $ alias put='put.sh user@domain.com bucketabcdefg'
+    $ TMP=`date +%s`; mkdir ~/tmp-$TMP; cd ~/tmp-$TMP
+    $ put ~/files/awesome.data
+    $ cd ~/
+    $ rm -rfv ~/tmp-*
+
---
+++ put.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+SIZE=134217728
+
+FILE=$3
+TO=$1
+BUCKET=$2
+
+if [ ! -f "$FILE" ]; then
+        echo "not a file"
+        exit 1
+fi
+
+SUM=$(shasum $FILE)
+
+cat > MANIFEST << EOF
+$SUM
+EOF
+
+echo $SUM
+
+PREFIX=$(date +%s)
+
+echo "Splitting $FILE"
+split --bytes=134217728 $FILE x
+
+echo "Encrypting MANIFEST"
+gpg --encrypt --recipient $TO MANIFEST
+
+for i in x*
+do
+        echo "Encrypting $i"
+        gpg --encrypt --recipient $TO $i
+done
+
+for i in *.gpg
+do
+        echo "Uploading part $i"
+        s3cmd put $i s3://$BUCKET/$PREFIX/
+done
+
+echo "Done, cleaning up"
+rm -rfv *
+
